fc2cd5e8429d901355dbbc74fb65cdd6f3423631
Boothe Davis <photoboothe@yahoo.com>
2018-06-11 11:58:07 -0400

    help

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,18 @@ def addition(num1,num2)
  num1+num2
 end
 
-def subtraction(num1)
+def subtraction(num1,num2)
 	num1-num2
 
 end
+
+def multiply(num1,num2)
+	num1*num2
+end
+	p multiply(4,2)
+
+def divide(num1,num2)
+num1/num2
+
+	end
+	p divide(6,2)
